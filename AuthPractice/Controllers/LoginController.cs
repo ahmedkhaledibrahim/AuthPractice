42e242887cf05fc86971c7c3a0802d51cdb9f14a
User <User@DESKTOP-U6FH8F4>
2024-10-16 17:02:15 +0300

    Add project files.

---
+++ AuthPractice/Controllers/LoginController.cs
@@ -0,0 +1,62 @@
+﻿using AuthPractice.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace AuthPractice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public LoginController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody] UserLogin userLogin)
+        {
+            var user = Authenticate(userLogin);
+            if(user != null)
+            {
+                var token = Generate(user);
+                return Ok(new { accessToken = token});
+            }
+            return NotFound("User not found");
+        }
+
+        private string Generate(UserModel user)
+        {
+            var securityKey = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Username),
+                new Claim(ClaimTypes.Email, user.EmailAddress),
+                new Claim(ClaimTypes.GivenName, user.GivenName),
+                new Claim(ClaimTypes.Role, user.Role),
+            };
+
+            var token = new JwtSecurityToken(_configuration["Jwt:Issuer"], _configuration["Jwt:Audience"], claims,expires:DateTime.Now.AddMinutes(15), signingCredentials: credentials);
+            
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private UserModel Authenticate(UserLogin userLogin)
+        {
+            var currentUser = UserConstants.Users.Where(u => u.Password == userLogin.Password && u.Username.ToLower() == userLogin.Username.ToLower()).FirstOrDefault();
+            if(currentUser != null)
+            {
+                return currentUser;
+            }
+            return null;
+        }
+    }
+}
---
+++ AuthPractice/Models/UserConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace AuthPractice.Models
+{
+    public class UserConstants
+    {
+        public static List<UserModel> Users = new List<UserModel>()
+        {
+            new UserModel(){ Username = "Ahmed Khaled",EmailAddress = "ahmed@gmail.com", Password = "Ahmed123", Role = "Admin",GivenName="Ahmed",Surname="Khaled"},
+            new UserModel(){ Username = "Khaled Ibrahim",EmailAddress = "khaled@gmail.com", Password = "Khaled123", Role = "User",GivenName="Khaled",Surname="Ibrahim"},
+            new UserModel(){ Username = "Mazen Mohamed",EmailAddress = "Mazen@gmail.com", Password = "MD123", Role = "Seller",GivenName="Mohamed",Surname="Mazen"}
+        };
+    }
+}
